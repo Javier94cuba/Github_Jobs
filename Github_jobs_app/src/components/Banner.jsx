75f51046663c4848507ae4aebd00a611cb90a116
Javier Lopéz <jlopezhdez94@gmail.com>
2023-07-05 18:42:25 +0200

    working banne

---
+++ Github_jobs_app/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import style from "./banner.css";
+
+export function Banner() {
+  return (
+    <div className={style.banner}>
+      <h1>Github Jobs</h1>
+    </div>
+  );
+}
--- Github_jobs_app/src/main.jsx
+++ Github_jobs_app/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Banner } from "./components/banner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello world</h1>
+    <Banner />
   </React.StrictMode>
 );
